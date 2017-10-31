4ad13efdf26794895a514e33c9a516feaf731962
nickroseth <33129694+nickroseth@users.noreply.github.com>
2017-10-31 15:53:41 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-// server.js
+var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000;
 
-// set up ========================
-var express = require('express');
-var app = express(); // create our app w/ express
-var fs = require('fs')
-var mongoose = require('mongoose'); // mongoose for mongodb
-var morgan = require('morgan'); // log requests to the console (express4)
-var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
-var database = require('./config/database'); //load the database config
 
-// configuration =================
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(morgan('combined')); // log every request to the console
-app.use(bodyParser.urlencoded({
-    'extended': 'true'
-})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({
-    type: 'application/vnd.api+json'
-})); // parse application/vnd.api+json as json
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test_db');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-mongoose.connect(database.url); // connect to mongoDB database
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-// load the routes
-require('./app/routes')(app);
+var h = require('./handler');
 
-// listen (start app with node server.js) ======================================
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.get("/", h.getHandler );
 
-module.exports = app;
+console.log(h.otherOne);
+
+var ctrl = require('./api/controllers/toDoListController');
+
+// List tasks
+app.get('/tasks', ctrl.list_all_tasks);
+
+// Create task
+app.post('/tasks', ctrl.create_task);
+
+
+/*
+
+/*
+var toDoController= require('../api/controllers/toDoListController');
+
+app.get("/",function(req,res){
+	res.send("hi");
+	console.log(req.get('Content-Type'));
+	}
+)
+*/
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
